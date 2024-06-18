a59f96a4ea494f7fcce5f1ece9dd260d2e94c6b6
Athis_Lingam <103132347+Athis182IT115@users.noreply.github.com>
2024-06-18 17:40:12 +0530

    Update sideapp.py

--- sideapp.py
+++ sideapp.py
@@ -1,4 +1,4 @@
 import streamlit as st
 # Set page title and icon
 st.set_page_config(page_title='Dhya Waterproofing', page_icon="logodhya.png")
-st.title("red:[Coming soon!!!]")
+st.title("__**:red[Coming soon...☔]**__")
